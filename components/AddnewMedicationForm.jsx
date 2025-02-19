bdd71eb000dd50a1232df62761edd02ed9d8d397
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-02-19 20:30:10 +0530

    Adding Medication functionalty added

--- components/AddnewMedicationForm.jsx
+++ components/AddnewMedicationForm.jsx
@@ -1,16 +1,24 @@
-import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, Alert, FlatList, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import Colors from '../constant/Colors'
 import { Ionicons } from '@expo/vector-icons'
 import { TypeList, WhenToTake } from '../constant/Options'
 import { Picker } from '@react-native-picker/picker'
 import RNDateTimePicker from '@react-native-community/datetimepicker'
-import { formatDateForText, FormatDateTime } from '../service/ConvertDateTime'
+import { formatDateForText, FormatDateTime, formatTime, getDatesRange } from '../service/ConvertDateTime'
+import { doc, setDoc } from 'firebase/firestore'
+import { db } from '../config/firebaseConfig'
+import { getLocalStorage } from '../service/storage'
+import { useRouter } from 'expo-router'
 
 export default function AddnewMedicationForm() {
+
+    const router = useRouter();
     const [formData, setformData] = useState();
     const [showStartDate, setshowStartDate] = useState(false);
     const [showEndDate, setshowEndDate] = useState(false);
+    const [showTimePicker, setshowTimePicker] = useState(false);
+    const [loading, setloading] = useState(false);
 
     const handleInputChange = (field, value) => {
         setformData(prev => ({
@@ -18,6 +26,42 @@ export default function AddnewMedicationForm() {
             [field]: value
         }));
     };
+
+    const SaveMedication = async () => {
+        try {
+            setloading(true);
+            const docId = Date.now().toString();
+            const user = await getLocalStorage('userDetails');
+            if (!(formData || formData?.name || formData?.type || formData?.dose || formData?.whenToTake || formData?.startDate || formData?.endDate || formData?.remainderTime)) {
+                ToastAndroid.show('Please fill all the fields', ToastAndroid.BOTTOM);
+                Alert.alert('Error', 'Please fill all the fields');
+                return;
+            }
+            const dates = getDatesRange(formData?.startDate, formData?.endDate);
+            await setDoc(doc(db, 'medication', docId), {
+                ...formData,
+                docId: docId,
+                user: user?.email,
+                dates: dates
+            });
+            ToastAndroid.show('Medication saved successfully', ToastAndroid.BOTTOM);
+            Alert.alert('Success', 'Medication saved successfully', [
+                {
+                    text: 'Ok',
+                    onPress: () => {
+                        router.replace('(tabs)');
+                    }
+                }
+            ]);
+            setformData();
+        }
+        catch (error) {
+            console.log(error)
+        }
+        finally {
+            setloading(false);
+        }
+    }
     return (
         <View style={styles.container}>
             {/* Medicine Name */}
@@ -89,26 +133,52 @@ export default function AddnewMedicationForm() {
                         }
                     </TouchableOpacity>
                 </View>
-                <View style={[{ flex: 1 }, styles.formContainer]}>
-                    <Text style={styles.label}>End Date</Text>
-                    <TouchableOpacity onPress={() => setshowEndDate(true)} style={styles.medicationWrapper}>
-                        <Ionicons name='calendar-outline' size={20} color={Colors.PRIMARY} style={styles.icon} />
-                        <Text style={styles.medicationTextInput}>{formData && formData.endDate ? formatDateForText(formData.endDate) : 'End Date'}</Text>
-                        {
-                            showEndDate &&
-                            <RNDateTimePicker minimumDate={new Date()} onChange={(event) => {
-                                handleInputChange('endDate', FormatDateTime(event.nativeEvent.timestamp))
-                                setshowEndDate(false)
-                            }} value={formData && formData.endDate ? new Date(formData.endDate) : new Date()} />
-                        }
-                    </TouchableOpacity>
-                </View>
+                {
+                    formData && formData.startDate && (
+                        <View style={[{ flex: 1 }, styles.formContainer]}>
+                            <Text style={styles.label}>End Date</Text>
+                            <TouchableOpacity onPress={() => setshowEndDate(true)} style={styles.medicationWrapper}>
+                                <Ionicons name='calendar-outline' size={20} color={Colors.PRIMARY} style={styles.icon} />
+                                <Text style={styles.medicationTextInput}>{formData && formData.endDate ? formatDateForText(formData.endDate) : 'End Date'}</Text>
+                                {
+                                    showEndDate &&
+                                    <RNDateTimePicker minimumDate={formData && formData.startDate ? new Date(formData.startDate) : new Date()} onChange={(event) => {
+                                        handleInputChange('endDate', FormatDateTime(event.nativeEvent.timestamp))
+                                        setshowEndDate(false)
+                                    }} value={formData && formData.endDate ? new Date(formData.endDate) : new Date()} />
+                                }
+                            </TouchableOpacity>
+                        </View>
+                    )
+                }
             </View>
 
-            {/* Set Remainder */}
-            
-            {/* Save Button */}
+            {/* Set Remainder input */}
+            <View style={styles.formContainer}>
+                <Text style={styles.label}>Set Remainder</Text>
+                <TouchableOpacity onPress={() => setshowTimePicker(true)} style={styles.medicationWrapper}>
+                    <Ionicons name='timer-outline' size={20} color={Colors.PRIMARY} style={styles.icon} />
+                    <Text style={styles.medicationTextInput}>{formData && formData.remainderTime ? formData.remainderTime : 'Set Remainder Time'}</Text>
+                    {
+                        showTimePicker &&
+                        <RNDateTimePicker mode='time' onChange={(event) => {
+                            handleInputChange('remainderTime', formatTime(event.nativeEvent.timestamp))
+                            setshowTimePicker(false)
+                        }} value={formData && formData.remainderTime ? new Date(formData.endDate) : new Date()} />
+                    }
+                </TouchableOpacity>
+            </View>
 
+            {/* Save Button */}
+            <TouchableOpacity style={styles.btn} onPress={SaveMedication}>
+                {
+                    loading ? (
+                        <ActivityIndicator size="small" color="white" />
+                    ) : (
+                        <Text style={styles.btnTxt}>Add New Medication</Text>
+                    )
+                }
+            </TouchableOpacity>
         </View>
     )
 }
@@ -161,5 +231,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: "100%",
         gap: 10
+    },
+    btn: {
+        backgroundColor: Colors.PRIMARY,
+        padding: 15,
+        alignItems: 'center',
+        borderRadius: 10,
+        marginTop: 20
+    },
+    btnTxt: {
+        color: 'white',
+        fontSize: 18
     }
 })
--- service/ConvertDateTime.jsx
+++ service/ConvertDateTime.jsx
@@ -1,9 +1,32 @@
 import moment from "moment"
 
 export const FormatDateTime = (timestamp) => {
-    return new Date(timestamp).setHours(0, 0, 0, 0)
+    return new Date(timestamp)
 }
 
 export const formatDateForText = (date) => {
     return moment(date).format('LL');
+}
+
+export const formatTime = (timestamp) => {
+    const date = new Date(timestamp);
+    const timeString = date.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+    })
+
+    return timeString;
+}
+
+export const getDatesRange = (startDate, endDate) => {
+    const start = moment(startDate, "DD-MM-YYYY");
+    const end = moment(endDate, "DD-MM-YYYY");
+    const dates = [];
+
+    while (start.isSameOrBefore(end)) {
+        dates.push(start.format("DD-MM-YYYY"));
+        start.add(1, "days");
+    }
+     
+    return dates;
 }
